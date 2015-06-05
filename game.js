fb29ede9106f38feb3f95c9e53ad844d7d03b667
Nigel <jollyra@outlook.com>
2015-06-04 22:59:42 -0700

    First commit.
    Html5 canvas scaffolding. Following http://www.html5rocks.com/en/tutorials/canvas/notearsgame/ as a guide.

---
+++ game.js
@@ -0,0 +1,6 @@
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
+
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
