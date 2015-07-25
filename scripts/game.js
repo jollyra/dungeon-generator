719a6d2e9a68aa1443b97a9518650775bd2e3bc4
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-07-25 10:41:46 -0700

    drawing the stage

--- scripts/game.js
+++ scripts/game.js
@@ -6,30 +6,68 @@ canvas.appendTo('body');
 
 
 // The stage is a 2d array of tiles.
-var y_max = 10;
-var x_max = 10;
-var tile_w = 10;
-var tile_h = 10;
-var stage =  new Array(this.y_max);
+var y_max = 5;
+var x_max = 5;
 var init =  function () {
+	var stage =  new Array(y_max);
 	_.fill(stage, _.fill(new Array(x_max), 0));
+	return stage;
 };
-var drawTile = function (x, y, colour) {
-	ctx.fillStyle = colour;
-	console.log(x, y, tile_w, tile_h);
-	ctx.fillRect(x, y, tile_w, tile_h);
+
+var drawTile = function (x, y, tile) {
+	var tile_w = 5;
+	var tile_h = 5;
+	var TILES = {
+		0: "#333300",  // Wall
+		1: "#D6C2AD"   // Floor
+	};
+	ctx.fillStyle = TILES[tile];
+	//console.log('x:', x * tile_w, 'y:', y * tile_h, 'x_w:', tile_w, 'y_w:', tile_h, 'tile:', TILES[tile]);
+	ctx.fillRect(x * tile_w, y * tile_h, tile_w, tile_h);
 };
-var draw = function () {
-	for(i = 0; i < y_max; i++) {
-		for(k = 0; k < y_max; k++) {
-			var c1 = "#FF0000";
-			var c2 = "#0000FF";
-			// Ternary operater for proof that iteration is working.
-			drawTile(k * tile_w, i * tile_h, (k*i % 2 === 0 ? c1 : c2));
+
+var drawStage = function (stage) {
+	for(i = 0; i < stage.y_max; i++) {
+		for(k = 0; k < stage.x_max; k++) {
+			var x = k;
+			var y = i;
+			var tile = stage.stage[y][x];
+			drawTile(x, y, tile);
 		}
 	}
 };
 
+var stage = {
+	_stage: [
+		[1, 1, 1, 1, 1],
+		[1, 0, 0, 0, 1],
+		[1, 0, 1, 0, 1],
+		[1, 0, 0, 0, 1],
+		[1, 1, 1, 1, 1]
+	],
+	x_max: function () {
+		if (this._stage && this._stage[0]) {
+			return this._stage[0].length;
+		} else {
+			throw new Error('Where\'s my GODDAMN array? _stage = ', this._stage);
+		}
+	},
+	y_max: function () {
+		if (this._stage) {
+			return this._stage.length;
+		} else {
+			throw new Error('Where\'s my GODDAMN array? _stage = ', this._stage);
+		}
+	},
+	getStage: function () {
+		return {
+			stage: this._stage,
+			y_max: this.y_max(),
+			x_max: this.x_max()
+		};
+	}
+}
 
-init();
-draw();
+
+drawStage(stage.getStage());
+
