63bbf384707e0b7175fbc3eb20c72be67c46f60a
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-08-03 09:35:10 -0700

    code quality

--- scripts/proceduralGen.js
+++ scripts/proceduralGen.js
@@ -1,7 +1,6 @@
 var x_m = 50;
 var y_m = 50;
 
-// Create a 2d array of zeros.
 function emptyStage(x, y) {
 	var stage =  new Array(y);
 	for(i = 0; i < y; i++) {
@@ -20,7 +19,7 @@ function randomRoom(stage) {
 		MIN_HEIGHT = 5,
 		h = evenize(_.random(MIN_HEIGHT, MAX_HEIGHT)),
 		w = evenize(_.random(MIN_WIDTH, MAX_WIDTH)),
-		x = oddRng(1, stage.x_max - w - 1),  // -1 for array bounds and -3 for padding
+		x = oddRng(1, stage.x_max - w - 1),
 		y = oddRng(1, stage.y_max - h - 1);
 	var room = { h: h, w: w, x: x, y: y };
 	if (x + w >= x_m || y + h >= y_m) { // TODO: This should be on the stage object
@@ -68,18 +67,13 @@ function placeRooms(stage, numTries) {
 		if (checkCollisionsOnStage(stage, room) === false) {
 			digRoom(stage, room);
 			rooms.push(room);
-		} else {
-			//console.log('placeRooms: room: ', room, 'has collision.');
 		}
 	}
 	return rooms;
 }
 
-// find some rock
-// start cutting a passage there
-// cut up, down, left, right at random
 function carvePassages(stage, x0, y0) {
-	var pos = {x: x0, y: x0};  // Current position initialized to the initial position.
+	var pos = {x: x0, y: x0};
 	dig(stage, pos.x, pos.y); // Excavate the starting tile.
 	var prevDig = Dig.down;  //TODO: should choose this randomly
 	delve();
