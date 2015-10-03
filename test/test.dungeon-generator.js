60ca52ca307bb90a71ecdbe05fc8b88048cd166b
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-10-02 22:11:57 -0700

    Execute a passing unit test

---
+++ test/test.dungeon-generator.js
@@ -0,0 +1,8 @@
+// Sample unit test using expect.js
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            expect([1,2,3].indexOf(5)).to.be(-1);
+        });
+    });
+});
